7928847ad99023349e04b3cb8bb6ff43810797b4
AbdullahElian1 <abodelian28@gmail.com>
2021-06-20 14:24:38 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import IsLoadingAndError from './IsLoadingAndError';
-import Footer from './footer';
+import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { withAuth0 } from '@auth0/auth0-react';
+// import login from './login';
+import myFavoriteBooks from './myFavoriteBooks';
+import Login from './login';
+
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,23 +19,28 @@ class App extends React.Component {
 
   render() {
     console.log('app', this.props)
+    const {isAuthenticated}  = this.props.auth0;
+
     return(
       <>
         <Router>
-          <IsLoadingAndError>
+          {/* <IsLoadingAndError> */}
             <Header />
               <Switch>
                 <Route exact path="/">
                   {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
+                  { (isAuthenticated ?  <myFavoriteBooks /> : <Login />) }
+
                 </Route>
                 {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
               </Switch>
             <Footer />
-          </IsLoadingAndError>
+          {/* </IsLoadingAndError> */}
         </Router>
       </>
     )
   }
 }
 
-export default App;
+export default  withAuth0(App);
+
--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import './header.css';
-
+import { withAuth0 } from '@auth0/auth0-react';
+import Loginbutton1 from './component/Loginbutton1';
+import Logoutbottun from './component/Logoutbottun'
 class Header extends React.Component {
+
+  // constructor(props){
+
+  // }
+  // const {isAuthenticated}  = this.props.auth0;
+
+
   render() {
+    const {isAuthenticated}  = this.props.auth0;
+
     return(
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
           <Link to="/">Home</Link>
           <Link to="/profile">Profile</Link>
           {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+          {/* <Loginbutton1 />
+          <Logoutbottun /> */}
+
+         { (isAuthenticated) ?  <Logoutbottun /> : <Loginbutton1 /> }
+         
+
       </Navbar>
     )
   }
 }
 
-export default Header;
+export default withAuth0(Header);
